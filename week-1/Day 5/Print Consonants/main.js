c121eb32287a24a49298888b0df29c0dd86de6fc
Ashish kumar Pal <ashishpal438@gmail.com>
2021-08-22 00:12:32 +0530

    created a function which checks if a character is consonant or vowel

---
+++ week-1/Day 5/Print Consonants/main.js
@@ -0,0 +1,20 @@
+function checkConsonant(char){
+
+switch(char) {
+  case "a":
+  case "e":
+  case "i":
+  case "o":
+  case "u":
+  case "A":
+  case "E":
+  case "I":
+  case "O":
+  case "U":
+  return("Vowel");
+  break;
+  default:
+  return("Consonant");
+}
+}
+console.log(checkConsonant("d"));
