83a35eba3aa5870bb2eeaa05bcf34a7cabed4d98
Ashish kumar Pal <ashishpal438@gmail.com>
2021-08-25 16:14:28 +0530

    Created a program to find the sum of odd numbers to a given limit using for loop.

---
+++ week-2/Day 1/Odd Sum till N/main.js
@@ -0,0 +1,13 @@
+function oddSumBelowN(num) {
+    // Write code here
+  var sum=0;
+    for(i=1;i<=num;i++){
+    if(i%2!==0){
+    sum = sum+i;
+    }
+    }
+  console.log(sum)
+}
+
+
+oddSumBelowN(10);
