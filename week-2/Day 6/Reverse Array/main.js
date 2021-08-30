01b43785116e83c73fd53b2cc7add914576c09f4
Ashish kumar Pal <ashishpal438@gmail.com>
2021-08-30 23:37:47 +0530

    To print a given array in reverse order.

---
+++ week-2/Day 6/Reverse Array/main.js
@@ -0,0 +1,15 @@
+function reverseArrayTraversal(n, arr){
+    for(var i=1;i<=1;i++){
+     var pattern = "";
+      
+      for(var j=1;j<=n;j++){
+      pattern = pattern + arr[n-j] + " ";
+        
+        
+      }
+      console.log(pattern) 
+    }
+    
+  }
+ console.log(reverseArrayTraversal(5,[1,2,3,4,5]));
+  
