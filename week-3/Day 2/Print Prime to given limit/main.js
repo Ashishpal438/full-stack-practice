3ac9eea8c97648b71b46efc2b12fc96d67c53676
Ashish kumar Pal <ashishpal438@gmail.com>
2021-09-02 11:41:07 +0530

    To print prime numbers from 2 to a given limit.

---
+++ week-3/Day 2/Print Prime to given limit/main.js
@@ -0,0 +1,23 @@
+function isPrime(num){
+    var count = 0;
+    for(var i=1;i<=num;i++){
+      if(num % i === 0){
+       count++;
+      }
+    }
+    if(count === 2){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  
+  // using the above function to print prime numbers to a given limit
+  var limit = 20;
+  for(var i=2;i<=limit;i++){
+    if(isPrime(i)){
+      console.log(i)
+    }
+  }
+  
