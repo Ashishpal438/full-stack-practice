a5a5a250671a204ca4f31cdbb15724dffdb92925
Ashish kumar Pal <ashishpal438@gmail.com>
2021-09-02 11:34:40 +0530

    A Program to check if the given number is prime or not.

---
+++ week-3/Day 2/Check Prime/main.js
@@ -0,0 +1,17 @@
+function isPrime(num){
+    var count = 0;
+    for(var i=1;i<=num;i++){
+      if(num % i === 0){
+       count++;
+      }
+    }
+    if(count === 2){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  
+  console.log(isPrime(3));
+  
