e22f296dc13afb0f11e1093af384f38ef8c48b16
Ashish kumar Pal <ashishpal438@gmail.com>
2021-09-06 00:39:27 +0530

    To find if a given number is divisible by 2 or 3 or by both.

---
+++ week-3/Day 6/Two and three/main.js
@@ -0,0 +1,14 @@
+function twoAndThree(num) {
+  for(var i=1;i<=num;i++){
+   if((i % 2 === 0) && (i % 3 === 0)){
+      console.log("Both");
+    } else if(i % 2 === 0){
+    console.log("Two");
+    }else if(i % 3 === 0){
+    console.log("Three")
+    } else{
+    console.log("None")
+    }
+  } 
+}
+twoAndThree(18)
