d54db5d89b0dea86e257c8ac3287c4b4565135d3
Ashish kumar Pal <ashishpal438@gmail.com>
2021-09-10 11:01:22 +0530

    Program to check if the previous highest record is broken or not

---
+++ week-4/Day 3/break record/main.js
@@ -0,0 +1,12 @@
+function breakingTheRecord(n, m) {
+    if(m > n){
+    console.log("Broken");
+    }
+    else if(m === n){
+    console.log("Wao")
+    }
+    else{
+    console.log("Not Yet")
+    }
+  }
+  breakingTheRecord(264,200)
