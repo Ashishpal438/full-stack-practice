7f3799170785ef4812f6b8cdd9756062f528c979
Ashish kumar Pal <ashishpal438@gmail.com>
2021-09-10 10:44:37 +0530

    Program to find the remainder according to a given condition

---
+++ week-4/Day 3/Division task/main.js
@@ -0,0 +1,14 @@
+function divisionTask(N) {
+    var x =  Math.floor(32/N)
+  if(x===0){
+  console.log("Too Low")
+  }
+  else if (N === 0 ){
+  console.log("-1")
+  }
+  else{
+  console.log(x);
+  }
+    
+}
+divisionTask(15)
