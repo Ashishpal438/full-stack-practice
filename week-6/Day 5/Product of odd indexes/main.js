7d6b4c49428c7c311823f324157b1db1c9806948
Ashish kumar Pal <ashishpal438@gmail.com>
2021-10-04 23:12:24 +0530

    Program to find the product of odd indexes of an array.

---
+++ week-6/Day 5/Product of odd indexes/main.js
@@ -0,0 +1,10 @@
+function productOfEvenIndexes(N, arr) {
+    var pro = 1;
+    for(var i=0; i<N; i++){
+        if(i%2===1){
+            pro = pro * arr[i]
+        }
+    }
+    console.log(pro)
+  }
+productOfEvenIndexes(4,[1,2,3,4])
