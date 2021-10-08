41c06787d3f166af9b44004e6b43b52d1e180935
Ashish kumar Pal <ashishpal438@gmail.com>
2021-10-08 23:58:42 +0530

    Program to shuffle a given string.

---
+++ week-7/Day 2/shuffle string/main.js
@@ -0,0 +1,13 @@
+var restoreString = function(s, indices) {
+    var ans = "";
+    for(var i=0; i<s.length; i++){
+        for(var j=0; j<indices.length; j++){
+            if(i===indices[j]){
+                 ans = ans + s[j]
+            }
+        }
+       
+    }
+    return ans;
+};
+console.log(restoreString("codeleet",[4,5,6,7,0,2,1,3]));
