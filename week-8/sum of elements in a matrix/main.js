bf5d5bb87ad81bfc1b36e08081ca15c1eab7efb4
Ashish kumar Pal <ashishpal438@gmail.com>
2021-10-14 22:41:42 +0530

    Program to find the sum of all the elements present in a matrix.

---
+++ week-8/sum of elements in a matrix/main.js
@@ -0,0 +1,12 @@
+function sumOfMatrix (N,M,Grid) {
+    var sum = 0;
+    for(var i=0; i<N; i++){
+        for(var j=0; j<M; j++){
+            sum = sum + Grid[i][j];
+        }
+    }
+     console.log(sum);
+
+}
+
+sumOfMatrix(2,3,[[1,0,1],[-8,9,-2]])
