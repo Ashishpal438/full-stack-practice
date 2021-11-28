b46bd53929f015a4755a5cc86a8b03a1244ccbe2
Ashish kumar Pal <ashishpal438@gmail.com>
2021-11-28 23:30:55 +0530

    Minimum element in rotated and sorted array.

---
+++ week-11/Day 5/min in rotated and sorted array/index.js
@@ -0,0 +1,47 @@
+function runProgram(input) {
+  input = input.trim().split("\n")
+  var n = +input[0]
+  var arr = input[1].trim().split(" ").map(Number)
+  findMin(arr,n)
+}
+
+function findMin(arr,n){
+ var low = 0
+ var high = n-1
+
+ while(low <= high ){
+     var mid = low + Math.floor((high-low)/2)
+     if(arr[mid] > arr[high]){
+         low = mid +1 
+     }
+     else if(arr[mid] < arr[high]){
+         high = mid
+     }
+     else if(arr[mid]=== arr[high]){
+       high--
+     }
+ }
+ console.log(arr[low]);
+}
+
+if (process.env.USERNAME === "Ashish Pal") {
+  runProgram(`10
+  4 6 7 9 10 -1 0 1 2 3`);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
